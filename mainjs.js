639e8907f6f9eeb4dd5f6191c13c2bde9dbdecad
seuchoi0531 <seuchoi0531@gmail.com>
2024-12-06 16:01:59 +0900

    can make and show equation, but should set "span", "var" and frac

--- mainjs.js
+++ mainjs.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         this.coefficient = coefficient;
         this.variables = variables;
     }
+    var equation = document.getElementById("equation");
     var solve = document.getElementById("solve");
     var integer_max = 4;
     var denominator_max = 4; // 나중에 설정할수 있는 옵션을 만들면, 최솟값을 2로하게 해야함
@@ -16,6 +17,72 @@ $(document).ready(function () {
     var integer_sum = integer_max * (integer_max + 1) / 2;
     var denominator_sum = denominator_max * (denominator_max - 1) / 2;
 
+    function addTerm(t1, t2) {
+        var t1_key_list, t1_value_list;
+        [t1_key_list, t1_value_list] = sortTerm(Object.keys(t1.variables), Object.values(t1.variables));
+        var t2_key_list, t2_value_list;
+        [t2_key_list, t2_value_list] = sortTerm(Object.keys(t2.variables), Object.values(t2.variables));
+        if (t1_key_list.length != t2_key_list.length) {
+            console.error("can't add");
+            return -1;
+        }
+        var checklist = new Array(t1_key_list.length).fill(false);
+        for (var k = 0; k < t1_key_list.length; k++) {
+            if (t1_key_list[k] == t2_key_list[k]) {
+                if (t1_value_list[k] == t2_key_list[k])
+                    checklist[k] = true;
+            }
+        }
+        var flag = true;
+        for (var k = 0; k < checklist.length; k++) {
+            if (!checklist[k])
+                flag = false;
+        }
+        if (!flag) {
+            console.error("can't add");
+            return -1;
+        }
+        return new Term(t1.coefficient + t2.coefficient, t1.variables);
+    }
+    function multiplyTerm(t1, t2) {
+        var t1_key_list, t1_value_list;
+        [t1_key_list, t1_value_list] = sortTerm(Object.keys(t1.variables), Object.values(t1.variables));
+        var t2_key_list, t2_value_list;
+        [t2_key_list, t2_value_list] = sortTerm(Object.keys(t2.variables), Object.values(t2.variables));
+        var result = new Term(t1.coefficient * t2.coefficient, t1.variables);
+        for (var k = 0; k < t2_key_list.length; k++) {
+            var flag = false;
+            for (var i = 0; i < Object.keys(result.variables).length; i++) {
+                if (t2_key_list[k] == Object.keys(result.variables)[i]) {
+                    result.variables[t2_key_list[k]] = t2_value_list[k] + Object.values(result.variables)[i];
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+                result.variables[t2_key_list[k]] = t2_value_list[k];
+        }
+        return result;
+    }
+    function termToString(t) {
+        var str = "";
+        var var_key_arr, var_value_arr;
+        [var_key_arr, var_value_arr] = sortTerm(Object.keys(t.variables), Object.values(t.variables));
+        if (var_key_arr.length == 0)
+            return t.coefficient;
+        if (t.coefficient != 1) {
+            if (t.coefficient != -1)
+                str += t.coefficient;
+            else
+                str += "-";
+        }
+        for (var i = 0; i < var_key_arr.length; i++) {
+            str += var_key_arr[i];
+            if (var_value_arr[i] != 1)
+                str += "<sup>" + var_value_arr[i] + "</sup>";
+        }
+        return str;
+    }
     function showTerm(t, div) {
         var str = "";
         var var_key_arr, var_value_arr;
@@ -165,7 +232,6 @@ $(document).ready(function () {
                 break;
             }
         }
-        console.log("int" + result);
         return result;
     }
     function randFrac() {
@@ -189,7 +255,6 @@ $(document).ready(function () {
                 break;
             }
         }
-        console.log("frac" + numerator + "/" + denominator);
         return numerator / denominator;
     }
     function randVar(arr) {
@@ -232,30 +297,33 @@ $(document).ready(function () {
         }
         return result;
     }
-    var algebra_list = ["(a+b)2", "(a-b)2", "(x+a)(x+b)", "(ax+b)(cx+d)",
-        "(x+a)(x+b)(x+c)", "(x-a)(x-b)(x-c)", "(a+b+c)2",
-        "(a+b)3", "(a-b)3", "(a+b)(a2-ab+b2)", "(a-b)(a2+ab+b2)",
-        "(a+b+c)(a2+b2+c2-ab-bc-ca)", "(a2+ab+b2)(a2-ab+b2)"];
+    var algebra_list = ["(a+b)^2", "(a-b)^2", "(x+a)(x+b)", "(ax+b)(cx+d)",
+        "(x+a)(x+b)(x+c)", "(x-a)(x-b)(x-c)", "(a+b+c)^2",
+        "(a+b)^3", "(a-b)^3", "(a+b)(a^2-ab+b^2)", "(a-b)(a^2+ab+b^2)",
+        "(a+b+c)(a^2+b^2+c^2-ab-bc-ca)", "(a^2+ab+b^2)(a^2-ab+b^2)"];
+    var develop_list = ["a^2+2ab+b^2", "a^2-2ab+b^2", "x^2+(a+b)x+ab", "acx^2+(ad+bc)x+bc",
+        "x^3+(a+b+c)x^2+(ab+bc+ca)x+abc", "x^3-(a+b+c)x^2+(ab+bc+ca)x-abc", "a^2+b^2+c^2+2ab+2bc+2ca",
+        "a^3+3a^2b+3ab^2+b^3", "a^3-3a^2b+3ab^2-b^3", "a^3+b^3", "a^3-b^3",
+        "a^3+b^3+c^3-3abc", "a^4+a^2b^2+b^4"
+    ];
     var algebra_list_length = algebra_list.length;
     var algebra_list_on = new Array(algebra_list_length).fill(true);
     var prob_num_in_eq = 0.2;
 
     function showEquation(eq, div) {
-        console.log(eq);
         var str = "";
         var arr = [false, false, false, false, false]; //a,b,c,d,x
         var termArr = new Array(5).fill(randTerm(0, 0));
         if (eq.includes("a"))
             arr[0] = true;
         if (eq.includes("b"))
-            arr[0] = true;
+            arr[1] = true;
         if (eq.includes("c"))
-            arr[0] = true;
+            arr[2] = true;
         if (eq.includes("d"))
-            arr[0] = true;
+            arr[3] = true;
         if (eq.includes("x"))
-            arr[0] = true;
-
+            arr[4] = true;
         if (!arr[2] && !arr[4]) { //ab
             if (Math.random() < vartype_rate) {
                 termArr[0] = randTerm(100, { a: -1 });
@@ -291,31 +359,95 @@ $(document).ready(function () {
             termArr[1] = randTerm(100, 0);
             termArr[4] = randTerm(1, { x: 1 });
         } else if (arr[3]) { //abcdx
-            termArr[0] = randTerm(100,0);
-            termArr[1] = randTerm(100,0);
-            termArr[2] = randTerm(100,0);
-            termArr[3] = randTerm(100,0);
+            termArr[0] = randTerm(100, 0);
+            termArr[1] = randTerm(100, 0);
+            termArr[2] = randTerm(100, 0);
+            termArr[3] = randTerm(100, 0);
             termArr[4] = randTerm(1, { x: 1 });
         } else { //xabc
             termArr[0] = randTerm(100, 0);
             termArr[1] = randTerm(100, 0);
             termArr[2] = randTerm(100, 0);
-            termArr[4] = randTerm(1, { x: 1 });
+            termArr[4] = new Term(1, { x: 1 });
         }
-        for(var k = 0;k<eq.length;k++){
-            if(eq[k] =="a")
-                str +=//여기부터해야함, termToString함수 만들어야할듯
+        var tmp = "";
+        var k = 0;
+        console.log("eq : " + eq);
+        console.log("termArr[0] : " + termToString(termArr[0]));
+        console.log("termArr[1] : " + termToString(termArr[1]));
+        console.log("termArr[2] : " + termToString(termArr[2]));
+        console.log("termArr[3] : " + termToString(termArr[3]));
+        console.log("termArr[4] : " + termToString(termArr[4]));
+        while (k < eq.length) {
+            if (eq[k] != "(" && eq[k] != ")" && eq[k] != "+" && eq[k] != "-") {
+                tmp += eq[k];
+            } else {
+                if (tmp != "") {
+                    var tmpTerm = new Term(1, {});
+                    for (var i = 0; i < tmp.length; i++) {
+                        if (tmp[i] == "a")
+                            tmpTerm = multiplyTerm(tmpTerm, termArr[0]);
+                        else if (tmp[i] == "b")
+                            tmpTerm = multiplyTerm(tmpTerm, termArr[1]);
+                        else if (tmp[i] == "c")
+                            tmpTerm = multiplyTerm(tmpTerm, termArr[2]);
+                        else if (tmp[i] == "d")
+                            tmpTerm = multiplyTerm(tmpTerm, termArr[3]);
+                        else if (tmp[i] == "x")
+                            tmpTerm = multiplyTerm(tmpTerm, termArr[4]);
+                        else if (tmp[i] == "^") {
+                            var q = 1;
+                            while (q < parseInt(tmp[i + 1])) {
+                                switch (tmp[i - 1]) {
+                                    case "a": tmpTerm = multiplyTerm(tmpTerm, termArr[0]); break;
+                                    case "b": tmpTerm = multiplyTerm(tmpTerm, termArr[1]); break;
+                                    case "c": tmpTerm = multiplyTerm(tmpTerm, termArr[2]); break;
+                                    case "d": tmpTerm = multiplyTerm(tmpTerm, termArr[3]); break;
+                                    case "x": tmpTerm = multiplyTerm(tmpTerm, termArr[4]); break;
+                                    default: break;
+                                }
+                                q++;
+                            }
+                            i++;
+                        } else {
+                            tmpTerm = multiplyTerm(tmpTerm, new Term(parseInt(tmp), 0));
+                            while (true) {
+                                if (isNaN(parseInt(tmp[i])))
+                                    break;
+                                i++;
+                            }
+                        }
+                    }
+                    if (tmpTerm.coefficient < 0) {
+                        if (str[str.length - 1] == "+")
+                            str = str.substring(0, str.length - 1);
+                        else if (str[str.length - 1] == "-") {
+                            str = str.substring(0, str.length - 1) + "+";
+                            tmpTerm = multiplyTerm(tmpTerm, new Term(-1, {}));
+                        }
+                    }
+                    str += termToString(tmpTerm);
+                    tmp = "";
+                    k--;
+                } else {
+                    str += eq[k];
+                }
+            }
+            k++;
+        }
+        if (tmp != "") {
+            if (tmp[0] == "^" && tmp.length == 2)
+                str += "<sup>" + tmp[1] + "</sup>";
         }
         div.innerHTML = str;
     }
     var used_algebra_list = [];
     for (var k = 0; k < algebra_list_length; k++) {
-        console.log(algebra_list_on[k]);
         if (algebra_list_on[k])
             used_algebra_list.push(k);
     }
     var show_algebra_num = Math.floor(Math.random() * (used_algebra_list.length - 1));
-    showEquation(algebra_list[show_algebra_num], solve);
+    showEquation(algebra_list[show_algebra_num], equation);
 
     /*
     var t = new Term(2 / 5, { z: 1, x: 4, y: 3 });
